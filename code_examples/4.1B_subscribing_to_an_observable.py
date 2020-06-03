673e8a23ecaf4accf9ef042431f86588ddf109fc
Brian Merritt <b.l.merritt@kent.ac.uk>
2020-06-03 23:11:37 +0100

    Working on Todo fixes

--- code_examples/4.1B_subscribing_to_an_observable.py
+++ code_examples/4.1B_subscribing_to_an_observable.py
@@ -3,9 +3,11 @@ from rx import create, from_callable
 #  TODO fix this - Alpha is generated but followed by Completed
 
 def MySubscriber(observer, scheduler):
-    #observer.on_next(next(iter(["Alpha", "Beta", "Gamma", "Delta", "Epsilon"]))) # Produces first item Alpha then stops
-    #observer.on_next(iter(["Alpha", "Beta", "Gamma", "Delta", "Epsilon"])) # Returns list iterator object, not items iterated
-    #observer.on_next(lambda: for item in ["Alpha", "Beta", "Gamma", "Delta", "Epsilon"]: yield item) # Syntax error - can't use for here, with or without lambda
+    observer.on_next("Alpha")
+    observer.on_next("Beta")
+    observer.on_next("Gamma")
+    observer.on_next("Delta")
+    observer.on_next("Epsilon")
     observer.on_completed()
 
 MyObserver = create(MySubscriber)
--- code_examples/6.4A_grouping_into_lists.py
+++ code_examples/6.4A_grouping_into_lists.py
@@ -1,8 +1,8 @@
-from rx import from_, operators as ops, of
+from rx import from_, operators as ops, range
 
 items = ["Alpha", "Beta", "Gamma", "Delta", "Epsilon"]
 
 from_(items).pipe(
     ops.group_by(lambda s: len(s)),
-    ops.flat_map(lambda grp: grp.to_list())  # Todo grp.to_list() of a groupedobservable is not working - fix it
+    ops.flat_map(lambda grp: grp.to_set()),  # Todo grp.to_list() of a groupedobservable is not working - fix it
 ).subscribe(lambda i: print(i))
