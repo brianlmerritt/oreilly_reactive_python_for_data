2cea764e92b99df2393b987889c5aed3b2ae386f
Brian Merritt <b.l.merritt@kent.ac.uk>
2020-06-15 13:27:57 +0100

    Fix flat_map with pipe to list

--- code_examples/6.4A_grouping_into_lists.py
+++ code_examples/6.4A_grouping_into_lists.py
@@ -1,8 +1,9 @@
-from rx import from_, operators as ops, range
+from rx import from_, of, from_iterable, operators as ops, create
 
 items = ["Alpha", "Beta", "Gamma", "Delta", "Epsilon"]
 
+
 from_(items).pipe(
-    ops.group_by(lambda s: len(s)),
-    ops.flat_map(lambda grp: grp.to_set()),  # Todo grp.to_list() of a groupedobservable is not working - fix it
+    ops.group_by(key_mapper=lambda s: len(s)),
+    ops.flat_map(lambda grp: grp.pipe(ops.to_list()))  
 ).subscribe(lambda i: print(i))
